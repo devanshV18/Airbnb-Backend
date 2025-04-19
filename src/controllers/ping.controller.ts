4e2f0ba6cb869e9cd9f1f09a8016ed2914f7cfac
Devansh <devanshverma024@gmail.com>
2025-04-19 05:46:02 +0530

    test

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -3,7 +3,7 @@ import logger from "../config/logger.config";
 
 export const pingHandler = async (req: Request, res: Response, next: NextFunction) => {
     logger.info("Ping request received");
-    res.status(200).json({ message: "Pong!" });
+    res.status(200).json({ message: "Ping Pong!" });
 }
 
 // 1. have some unique id generator
