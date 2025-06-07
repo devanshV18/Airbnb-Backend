2b34a9075ce391f4ef075b6ff3c7f9cc832403ac
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-06-08 00:45:30 +0530

    added validation to post request as well.

--- src/routers/v1/hotel.router.ts
+++ src/routers/v1/hotel.router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { createHotelHandler, getHotelByIdHandler } from '../../controllers/hotel.controller';
+import { validateRequestBody } from '../../validators';
+import { hotelSchema } from '../../validators/hotel.validator';
 
 const hotelRouter = express.Router()
 
-hotelRouter.post('/', createHotelHandler);
+hotelRouter.post('/', validateRequestBody(hotelSchema), createHotelHandler);
 hotelRouter.get('/:id', getHotelByIdHandler);
 
 export default hotelRouter;
